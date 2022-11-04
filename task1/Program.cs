0dc0af115cf0921deeea6cb69240b8a3a39e42d2
Natalia <dgared16@icloud.com>
2022-11-04 11:39:57 +0100

    3

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("введите 5-ти значное число ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int rev = 0;
+int dig;
+while(num > 0)
+{
+    dig = num % 10;
+    rev = rev * 10 + dig;
+    num = num / 10;
+}
+
+if(temp == rev)
+{
+Console.WriteLine (" число палиндром ");
+}
+
+else
+{
+Console.WriteLine (" число не палиндром ");
+}
